5008355c623f2dfac79d46013bb50b4654ea1c83
@furiyandev <furiyanmaravilla@gmail.com>
2022-12-12 18:05:22 +0800

    exercise

--- frian.java
+++ frian.java
@@ -5,5 +5,14 @@ public class frian {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] list = new int[5];
+
+        System.out.println("Enter a number: ");
+        for (int i = 0; i < list.length; i++) {
+            int input = sc.nextInt()
+        }
+
+        
+
     }
 }
