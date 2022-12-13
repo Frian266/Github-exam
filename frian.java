e4fca1b6ed461cfe4dfac9318629e96b209a02e0
@furiyandev <furiyanmaravilla@gmail.com>
2022-12-13 10:53:45 +0800

    final

--- frian.java
+++ frian.java
@@ -5,21 +5,13 @@ public class frian {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] list = new int[5];
+        // product code 
+        String[][] list = new String[5][1];
 
-        System.out.println("Enter a number: ");
         for (int i = 0; i < list.length; i++) {
-            int input = sc.nextInt();
-        }
-        int max = 0;
-        for (int i = 0; i < list.length; i++) {
-            for (int j = 1; j < list.length; j++) {
-                int num1 = list[i] , num2 = list[j]; 
-                max = Math.max(num1 , num2);
+            for (int j = 1; j < 2; j++) {
+                
             }
         }
-
-        System.out.println(max);
- 
     }
 }
