7a1bfb3bc354ee49dc914ea5ca5ce819bd6b0eac
furiyan_gab <frianpogi662@gmail.com>
2022-12-15 11:05:56 +0800

    draft

--- frian.java
+++ frian.java
@@ -21,21 +21,20 @@ public class frian {
         int quanti = sc.nextInt();
         System.out.println("");
         for (int i = 0; i < list.length; i++) {
-            for (int j = 1; j < 2; j++) {
 
-                 if (input.equals(list[i][0])) {
-                    System.out.println("Item is found. ");
-                    System.out.println("Product code: " + list[i][0]);
-                    System.out.println("Description: " + list[i][1]);
-                    System.out.println("Price: " + list[i][2]);
-                    System.out.println("Quantity: " + quanti);
-                    double price = Double.valueOf(list[i][2]);
-                    double amount = quanti * price;
-                    System.out.println("Amount: " + amount + "0");
-                    System.out.println("");
+            if (input.equals(list[i][0])) {
+                System.out.println("Item is found. ");
+                System.out.println("Product code: " + list[i][0]);
+                System.out.println("Description: " + list[i][1]);
+                System.out.println("Price: " + list[i][2]);
+                System.out.println("Quantity: " + quanti);
+                double price = Double.valueOf(list[i][2]);
+                double amount = quanti * price;
+                System.out.println("Amount: " + amount + "0");
+                System.out.println("");
                     System.out.println("Thank you for your business!");
-                } 
-            }
+            } 
+            
         }
     }
 }
